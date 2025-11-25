7f74d2610bc4d0b76e99dc5e8248fa35c6c0bba6
lziolek <lubomirziolek>
2025-11-25 15:46:02 +0100

    lab5

--- foo.hpp
+++ foo.hpp
@@ -8,5 +8,16 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    std::vector<char> lubi_zwierze;
+    for (auto &p : people)
+    {
+        p.birthday();
+        if(p.isMonster())
+        {
+            lubi_zwierze.push_back('n');
+        }
+        else {lubi_zwierze.push_back('y');}
+    }
+    std::reverse(lubi_zwierze.begin(),lubi_zwierze.end());
+    return {lubi_zwierze};
 }
